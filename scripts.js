37d2de63b153c0ee0a2dbb84142d57c8931baf62
Jamie Telford <Jamie.Telford@FreelanceAnimation.local>
2017-07-28 13:12:08 +1200

    edit script

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@ $(function() {
     var moveLeft = -10;
     var moveDown = 20;
         
-    var pyside = 'https://srinikom.github.io/pyside-docs/PySide/QtGui/';
+    var pyside = 'content/pyside-docs/PySide/QtGui/';
+    var maya_python = 'content/docs/maya/CommandsPython/';
     
         
     $('#pyside').hover(function(e) {
@@ -22,7 +23,7 @@ $(function() {
     $('a#pycom').hover(function(e) {
         var a_href = $(this).attr('href').split('#');
         
-        $.get(a_href[0], function(response) {
+        $.get(maya_python + a_href[0], function(response) {
                 $('#pop-up').html($(response).filter('#synopsis').html().split('<br>')[0]);
             });
         
